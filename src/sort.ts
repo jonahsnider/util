0d9134292b75b19ba1e11191a45340818174f8ef
Jonah Snider <jonah@jonah.pw>
2020-12-01 15:54:32 -0800

    docs(sort): improve documentation of sort functions

--- src/sort.ts
+++ src/sort.ts
@@ -1,7 +1,17 @@
 /**
  * Sort an array in ascending order (least to greatest).
+ * 
+ * @example
+ * ```
+ * const array = [5, 3, 2, 4, 1];
+ * 
+ * array.sort(ascending);
+ * ```
+ * 
  * @param a - Element
  * @param b - Element
+ * 
+ * @returns A negative value if first argument is less than second argument, zero if they're equal and a positive value otherwise
  */
 export function ascending(a: number, b: number): number {
 	return a - b;
@@ -9,8 +19,17 @@ export function ascending(a: number, b: number): number {
 
 /**
  * Sort an array in descending order (greatest to least).
+ * @example
+ * ```
+ * const array = [5, 3, 2, 4, 1];
+ * 
+ * array.sort(descending);
+ * ```
+ * 
  * @param a - Element
  * @param b - Element
+ * 
+ * @returns A negative value if first argument is less than second argument, zero if they're equal and a positive value otherwise
  */
 export function descending(a: number, b: number): number {
 	return b - a;
