2909b8ae8974f31f2815f9604649140109172649
Jonah Snider <jonah@jonah.pw>
2021-01-20 17:49:52 -0800

    style: fix indentation

--- src/identical.ts
+++ src/identical.ts
@@ -68,7 +68,7 @@ export function identicalManual<V, K = never>(
 		return a.every((item, i) => item === (b as typeof a)[i]);
 	}
 
-  // Horrible hack to make the tests specifically compile
+	// Horrible hack to make the tests specifically compile
 	if ((a as {size: number}).size !== (b as Set<V> | Map<K, V>).size) {
 		return false;
 	}
