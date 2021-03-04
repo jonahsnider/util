da9349328a1b0cc412b7ba5b0f2f0d738cef1f5d
Jonah Snider <jonah@jonah.pw>
2021-03-03 19:26:07 -0800

    perf(toDigits): stop stringifying and parsing the value parameter

--- src/toDigits.ts
+++ src/toDigits.ts
@@ -14,5 +14,7 @@
  * @returns Number rounded to the specified number of digits
  */
 export function toDigits(value: number, precision: number): number {
-	return Number(value.toFixed(precision));
+	const factor = 10 ** precision;
+
+	return Math.round(value * factor * (1 + Number.EPSILON)) / factor;
 }
