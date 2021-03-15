2ced84d0c2f8c8091a82fc25cf300013cfa2e0ed
Jonah Snider <jonah@jonah.pw>
2021-03-14 18:13:32 -0700

    feat(backoff): add exponential backoff controller

--- src/backoff.ts
+++ src/backoff.ts
@@ -1,4 +1,5 @@
 import {EventEmitter} from 'events';
+import {random} from './math';
 import {TypedEventEmitter} from './typedEventEmitter';
 
 export class Backoff
@@ -27,5 +28,44 @@ export class Backoff
 		this.emit('finish');
 
 		this.removeAllListeners('attempt');
+	}
+}
+
+/**
+ * An exponential backoff controller.
+ */
+export class ExponentialBackoff extends Backoff {
+	jitterAdjusted: {min: number; max: number};
+	timer: NodeJS.Timeout;
+
+	/**
+	 * Create an exponential backoff controller with a specified base delay.
+	 * @param baseDelay - Base delay in milliseconds
+	 * @param jitter - The minimum and maximum jitter percentages.
+	 */
+	constructor(private readonly baseDelay: number, jitter: {min: number; max: number} = {min: 0, max: 0}) {
+		super();
+
+		this.jitterAdjusted = {min: jitter.min + 1, max: jitter.max + 1};
+		this.timer = this.newTimer();
+	}
+
+	private get delay() {
+		return this.baseDelay * random(this.jitterAdjusted.min, this.jitterAdjusted.max) * 2 ** this.attempts;
+	}
+
+	private newTimer() {
+		return setTimeout(() => {
+			this.attempts++;
+
+			this.timer = this.newTimer();
+			this.emit('attempt');
+		}, this.delay);
+	}
+
+	public finish() {
+		clearTimeout(this.timer);
+
+		super.finish();
 	}
 }
