89362a76f1ce96cc6a4390f02677e30ab5d1618c
Jonah Snider <jonah@jonahsnider.com>
2021-06-12 17:09:48 -0700

    test(sort): add duplicate values to increase coverage of sort functions

--- src/sort.test.ts
+++ src/sort.test.ts
@@ -12,36 +12,36 @@ describe('sortObject', () => {
 
 describe('descending', () => {
 	it('sorts', () => {
-		const array = [2, 1, 3];
+		const array = [2, 3, 1, 3];
 
 		array.sort(descending);
 
-		expect(array).toStrictEqual([3, 2, 1]);
+		expect(array).toStrictEqual([3, 3, 2, 1]);
 	});
 
 	it('sorts bigints', () => {
-		const array = [2n, 1n, 3n];
+		const array = [2n, 3n, 1n, 3n];
 
 		array.sort(descending);
 
-		expect(array).toStrictEqual([3n, 2n, 1n]);
+		expect(array).toStrictEqual([3n, 3n, 2n, 1n]);
 	});
 });
 
 describe('ascending', () => {
 	it('sorts', () => {
-		const array = [2, 1, 3];
+		const array = [2, 3, 1, 3];
 
 		array.sort(ascending);
 
-		expect(array).toStrictEqual([1, 2, 3]);
+		expect(array).toStrictEqual([1, 2, 3, 3]);
 	});
 
-  it('sorts bigints', () => {
-		const array = [2n, 1n, 3n];
+	it('sorts bigints', () => {
+		const array = [2n, 3n, 1n, 3n];
 
 		array.sort(ascending);
 
-		expect(array).toStrictEqual([1n, 2n, 3n]);
+		expect(array).toStrictEqual([1n, 2n, 3n, 3n]);
 	});
 });
