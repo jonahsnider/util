5f9819b03fc0cb6c242436cc08c9199f17e0e75d
Jonah Snider <jonah@jonahsnider.com>
2021-06-15 08:45:04 -0700

    fix(object): improve typesafety of rename function

--- src/object.test.ts
+++ src/object.test.ts
@@ -8,5 +8,21 @@ describe(rename.name, () => {
 
 		// @ts-expect-error
 		expect(rename({a: 1, c: 2}, 'b', 'a')).toStrictEqual({a: undefined, c: 2});
+
+		const key = Math.random() < 0.5 ? 'a' : 'b';
+
+		rename(
+			{a: 1, c: 2},
+			'a',
+			// @ts-expect-error
+			key
+		);
+
+		rename(
+			{a: 1, b: 2},
+			// @ts-expect-error
+			key,
+			'c'
+		);
 	});
 });
--- src/object.ts
+++ src/object.ts
@@ -1,3 +1,6 @@
+/** @returns `T` if `T` is not a union type, `never` otherwise. */
+type NonUnion<T, U extends T = T> = (T extends T ? (U extends T ? false : true) : never) extends false ? T : never;
+
 /**
  * Create a copy of an object with a key renamed.
  *
@@ -14,16 +17,18 @@
  *
  * @returns A shallow-copied object with the key name updated
  */
-export function rename<T extends Record<PropertyKey, unknown>, K1 extends keyof T, K2 extends PropertyKey>(
-	object: T,
-	oldKey: K1,
-	newKey: K1 | K2
-): Omit<T, K1 | K2> & Record<K2, T[K1]> {
+export function rename<T, K1 extends keyof T, K2 extends PropertyKey>(
+	target: T,
+	oldKey: NonUnion<K1>,
+	newKey: NonUnion<K2>
+): Omit<T, K1 | K2> & Record<K2, T[K1]>;
+export function rename<T, K1 extends keyof T, K2 extends PropertyKey>(object: T, oldKey: K1, newKey: K1 | K2): Omit<T, K1 | K2> & Record<string, T[K1]> {
 	if (oldKey === newKey) {
 		return {...object} as any;
 	}
 
 	const {[oldKey]: _, ...result} = {...object, [newKey]: object[oldKey]};
 
+	// This logic can be rewritten to remove the need for a type assertion, but would require 2 iterations over result instead of 1
 	return result as any;
 }
