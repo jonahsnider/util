bd2d66f1aedd7bd1191ea3857d4045a60eaabfc9
Jonah Snider <jonah@jonahsnider.com>
2021-06-16 16:07:13 -0700

    docs(math): write iterable like a normal word in mode function

--- src/math.ts
+++ src/math.ts
@@ -105,7 +105,7 @@ export function median<T extends number>(array: readonly T[]): T {
 }
 
 /**
- * Calculate the mode of an `Iterable`.
+ * Calculate the mode of an iterable.
  * Strict equality (`===`) is used to compare elements.
  *
  * @example
