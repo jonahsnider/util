86ad8fb52a9bd94ef7a53a2da78d9f58fdc18f73
Jonah Snider <jonah@jonahsnider.com>
2021-06-16 07:20:00 -0700

    feat(curry): add invert function

--- src/curry.test.ts
+++ src/curry.test.ts
@@ -1,4 +1,4 @@
-import {not, nullish} from './';
+import {not, nullish, invert} from './';
 
 describe(not.name, () => {
 	it('negates booleans', () => {
@@ -6,5 +6,27 @@ describe(not.name, () => {
 		expect(not(() => false)()).toBe(true);
 
 		expect([0, null, '', undefined, false].filter(not(nullish))).toStrictEqual([0, '', false]);
+	});
+});
+
+function sort(a: number, b: number) {
+	return a - b;
+}
+
+describe(invert.name, () => {
+	it('inverts numbers', () => {
+		expect(invert(() => 1)()).toBe(-1);
+		expect(invert(() => -1)()).toBe(1);
+		expect(invert(() => 0)()).toBe(-0);
+		expect(invert(() => -0)()).toBe(0);
+
+		expect(invert(sort)(10, 5)).toBe(-5);
+	});
+
+	it('inverts bigints', () => {
+		expect(invert(() => 1n)()).toBe(-1n);
+		expect(invert(() => -1n)()).toBe(1n);
+		expect(invert(() => 0n)()).toBe(-0n);
+		expect(invert(() => -0n)()).toBe(0n);
 	});
 });
--- src/curry.ts
+++ src/curry.ts
@@ -15,3 +15,25 @@
 export function not<T extends (...params: any[]) => boolean>(fn: T): T {
 	return ((...params) => !fn(...params)) as T;
 }
+
+/**
+ * Create a new function taht calls the provided `fn` and then inverts the sign of the result.
+ *
+ * @example
+ * ```ts
+ * function sort(a, b) {
+ *   return a - b;
+ * }
+ *
+ * const inverted = invert(sort);
+ *
+ * inverted(10, 5); // -5
+ * ```
+ *
+ * @param fn - Function to invert return value of
+ *
+ * @returns The inverted return value of `fn`
+ */
+export function invert<T extends (...params: any[]) => number | bigint>(fn: T): T {
+	return ((...params) => -fn(...params)) as T;
+}
