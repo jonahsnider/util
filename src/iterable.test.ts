a058f767606d694dff66d27b05cd3c05d21861ce
Jonah Snider <jonah@jonahsnider.com>
2021-07-31 21:19:42 -0700

    test(iterable): reorder tests

--- src/iterable.test.ts
+++ src/iterable.test.ts
@@ -1,17 +1,17 @@
 import {combineIterables, every, find, includes, join, some} from './iterable';
 
-describe(join.name, () => {
-	it('joins elements', () => {
-		expect(join(['a', 'b', 'c'])).toStrictEqual('a,b,c');
-		expect(join(['a', 'b', 'c'], '|')).toStrictEqual('a|b|c');
-	});
-});
-
 describe(combineIterables.name, () => {
 	it('combines iterables', () => {
 		expect([...combineIterables([])]).toStrictEqual([]);
 		expect([...combineIterables([1])]).toStrictEqual([1]);
 		expect([...combineIterables([1], [2])]).toStrictEqual([1, 2]);
+	});
+});
+
+describe(join.name, () => {
+	it('joins elements', () => {
+		expect(join(['a', 'b', 'c'])).toStrictEqual('a,b,c');
+		expect(join(['a', 'b', 'c'], '|')).toStrictEqual('a|b|c');
 	});
 });
 
