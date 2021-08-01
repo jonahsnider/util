1a79bce9e5ffa16f413199dfc3f855c28e4ac827
Jonah Snider <jonah@jonahsnider.com>
2021-07-31 21:17:27 -0700

    feat(iterable): add `every` function

--- src/iterable.test.ts
+++ src/iterable.test.ts
@@ -1,4 +1,4 @@
-import {combineIterables, join} from './iterable';
+import {combineIterables, every, join} from './iterable';
 
 describe(join.name, () => {
 	it('joins elements', () => {
@@ -12,5 +12,17 @@ describe(combineIterables.name, () => {
 		expect([...combineIterables([])]).toStrictEqual([]);
 		expect([...combineIterables([1])]).toStrictEqual([1]);
 		expect([...combineIterables([1], [2])]).toStrictEqual([1, 2]);
+	});
+});
+
+describe(every.name, () => {
+	it('returns true if every element passes the predicate', () => {
+		expect(every([1, 2, 3] as unknown[], (x): x is number => typeof x === 'number')).toBe(true);
+		expect(every([1, 2, 3], () => true)).toBe(true);
+	});
+
+	it('returns false if some elements fail the predicate', () => {
+		expect(every([1, '2', 3], (x): x is number => typeof x === 'number')).toBe(false);
+		expect(every([1, 2, 3], () => false)).toBe(false);
 	});
 });
--- src/iterable.ts
+++ src/iterable.ts
@@ -54,3 +54,36 @@ export function join<T>(iterable: Iterable<T>, separator = ','): string {
 		}
 	}
 }
+
+/**
+ * Determines whether all the elements of an iterable are truthy.
+ *
+ * @example
+ * ```js
+ * every([false]); // false
+ * ```
+ * @example
+ * ```js
+ * every([false, true]); // false
+ * ```
+ * @example
+ * ```js
+ * every([true]); // true
+ * ```
+ *
+ * @param iterable - The iterable to take elements from
+ * @param predicate - The function to call on each element in the iterable to determine if it passes the test
+ *
+ * @returns `true` if every element is truthy, `false` otherwise
+ */
+export function every<T, S extends T>(iterable: Iterable<T>, predicate: (element: T) => element is S): iterable is Iterable<S>;
+export function every<T>(iterable: Iterable<T>, predicate: (element: T) => unknown): boolean;
+export function every<T>(iterable: Iterable<T>, predicate: (element: T) => unknown): boolean {
+	for (const element of iterable) {
+		if (!predicate(element)) {
+			return false;
+		}
+	}
+
+	return true;
+}
