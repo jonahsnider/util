16ea3a3ed38ce4a00a96665095fb0ca17b5dfde2
Jonah Snider <jonah@jonahsnider.com>
2021-07-31 23:25:16 -0700

    test(iterable): fix tests for `some` function

--- src/iterable.test.ts
+++ src/iterable.test.ts
@@ -32,8 +32,8 @@ describe(some.name, () => {
 		expect(some([1, 2, 3], x => x === 2)).toBe(true);
 	});
 
-	it('returns true if any element passes the predicate', () => {
-		expect(some([1, 2, 3], x => x === 2)).toBe(true);
+	it('returns false if every element fails the predicate', () => {
+  expect(some([1, 2, 3], x => x === 4)).toBe(false);
 	});
 });
 
