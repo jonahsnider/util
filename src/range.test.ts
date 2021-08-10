bc316d80086fe36b731569eeec1c4feba675dba0
Jonah Snider <jonah@jonahsnider.com>
2021-08-10 00:15:33 -0700

    test(range): use dynamic names

--- src/range.test.ts
+++ src/range.test.ts
@@ -5,13 +5,13 @@ describe(Range.name, () => {
 		expect(() => new Range(2, 1)).toThrow(RangeError);
 	});
 
-	describe('Range.from', () => {
+	describe(`${Range.name}.${Range.from.name}`, () => {
 		it('creates a range', () => {
 			expect(Range.from([1, 2])).toStrictEqual(new Range(1, 2));
 		});
 	});
 
-	describe('Range#has', () => {
+	describe(`${Range.name}#${Range.prototype.has.name}`, () => {
 		it('works with values in range', () => {
 			expect(new Range(1, 3).has(2)).toBe(true);
 			expect(new Range(1, 3n).has(2)).toBe(true);
