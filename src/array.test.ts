c5d09faf4ff4172fc44c6015e211a15779302b7c
Jonah Snider <jonah@jonahsnider.com>
2021-08-21 18:02:04 -0700

    test(array): improve test strictness

--- src/array.test.ts
+++ src/array.test.ts
@@ -13,7 +13,7 @@ describe(sample.name, () => {
 		expect(sample([1])).toBe(1);
 		expect(sample([])).toBe(undefined);
 		const letters = ['a', 'b', 'c'];
-		expect(sample(letters)).toStrictEqual(expect.anything());
+		expect(letters).toContain(sample(letters));
 	});
 });
 
@@ -196,24 +196,24 @@ describe(replace.name, () => {
 	it('replaces elements from an array', () => {
 		const array = [1, 2, 3];
 
-		expect(replace(array, 1, 2)).toStrictEqual(0);
+		expect(replace(array, 1, 2)).toBe(0);
 		expect(array).toStrictEqual([2, 2, 3]);
 
-		expect(replace(array, 1, 2)).toStrictEqual(-1);
+		expect(replace(array, 1, 2)).toBe(-1);
 		expect(array).toStrictEqual([2, 2, 3]);
 	});
 
 	it('works with empty arrays', () => {
 		const array: never[] = [];
 
-		expect(replace<number>(array, 1, 2)).toStrictEqual(-1);
+		expect(replace<number>(array, 1, 2)).toBe(-1);
 		expect(array).toStrictEqual([]);
 	});
 
 	it("works when element isn't found", () => {
 		const array = [1, 2, 3];
 
-		expect(replace(array, 4, 2)).toStrictEqual(-1);
+		expect(replace(array, 4, 2)).toBe(-1);
 		expect(array).toStrictEqual([1, 2, 3]);
 	});
 });
@@ -222,24 +222,24 @@ describe(replaceAll.name, () => {
 	it('replaces all elements from an array', () => {
 		const array = [1, 2, 1, 3];
 
-		expect(replaceAll(array, 1, 2)).toStrictEqual(2);
+		expect(replaceAll(array, 1, 2)).toBe(2);
 		expect(array).toStrictEqual([2, 2, 2, 3]);
 
-		expect(replaceAll(array, 1, 2)).toStrictEqual(0);
+		expect(replaceAll(array, 1, 2)).toBe(0);
 		expect(array).toStrictEqual([2, 2, 2, 3]);
 	});
 
 	it('works with empty arrays', () => {
 		const array: never[] = [];
 
-		expect(replaceAll<number>(array, 1, 2)).toStrictEqual(0);
+		expect(replaceAll<number>(array, 1, 2)).toBe(0);
 		expect(array).toStrictEqual([]);
 	});
 
 	it("works when element isn't found", () => {
 		const array = [1, 2, 3];
 
-		expect(replaceAll(array, 4, 2)).toStrictEqual(0);
+		expect(replaceAll(array, 4, 2)).toBe(0);
 		expect(array).toStrictEqual([1, 2, 3]);
 	});
 });
