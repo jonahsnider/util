a190c96bef165b87d0e42c0b9b1d2793fb4275e1
Jonah Snider <jonah@jonahsnider.com>
2021-09-11 21:43:44 -0700

    feat(array): add `pullAll` function

--- src/array.test.ts
+++ src/array.test.ts
@@ -12,6 +12,7 @@ import {
 	padEnd,
 	padStart,
 	pull,
+	pullAll,
 	replace,
 	replaceAll,
 	sample,
@@ -205,6 +206,32 @@ describe(pull.name, () => {
 		const array = [1, 2, 3];
 
 		expect(pull(array, 4)).toStrictEqual([]);
+		expect(array).toStrictEqual([1, 2, 3]);
+	});
+});
+
+describe(pullAll.name, () => {
+	it('pulls all elements from an array', () => {
+		const array = [1, 2, 1, 2, 2, 3, 2, 3];
+
+		expect(pullAll(array, 2)).toStrictEqual([2, 2, 2, 2]);
+		expect(array).toStrictEqual([1, 1, 3, 3]);
+
+		expect(pullAll(array, 2)).toStrictEqual([]);
+		expect(array).toStrictEqual([1, 1, 3, 3]);
+	});
+
+	it('works with empty arrays', () => {
+		const array: never[] = [];
+
+		expect(pullAll(array, 2)).toStrictEqual([]);
+		expect(array).toStrictEqual([]);
+	});
+
+	it("works when element isn't found", () => {
+		const array = [1, 2, 3];
+
+		expect(pullAll(array, 4)).toStrictEqual([]);
 		expect(array).toStrictEqual([1, 2, 3]);
 	});
 });
--- src/array.ts
+++ src/array.ts
@@ -271,6 +271,43 @@ export function pull<T>(array: T[], element: T): ReturnType<typeof array['splice
 }
 
 /**
+ * Remove all elements equal to a given element from an array.
+ * Strict equality (`===`) is used to compare elements.
+ *
+ * @example
+ * ```js
+ * const array = [1, 1, 2, 2, 3, 3];
+ *
+ * pullAll(array, 2); // [2, 2]
+ * console.log(array); // [1, 1, 3, 3]
+ *
+ * pullAll(array, 2); // []
+ * console.log(array); // [1, 1, 3, 3]
+ * ```
+ *
+ * @param array - The array to remove elements from
+ * @param element - The element to remove
+ *
+ * @returns The return value of `Array.prototype.splice`
+ */
+export function pullAll<T>(array: T[], element: T): ReturnType<typeof array['splice']> {
+	const indexes = indexOfAll(array, element);
+
+	// Reversing the indexes means we modify from end to start which has 2 benefits:
+  // - We don't have to worry about shifting indexes as we splice elements
+  // - We can avoid re-allocating the array as often
+	indexes.reverse();
+
+	const pulled: T[] = [];
+
+	for (const index of indexes) {
+		pulled.push(array.splice(index, 1)[0]);
+	}
+
+	return pulled;
+}
+
+/**
  * Replace the first occurrence of `searchElement` with `replacement` in an array.
  * Strict equality (`===`) is used to compare elements.
  *
