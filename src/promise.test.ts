1bd2bdf389ec22f335944f8177748e2b2eb59cf8
Jonah Snider <jonah@jonahsnider.com>
2021-09-11 18:34:27 -0700

    feat(promise): add `timeout` function

--- src/promise.test.ts
+++ src/promise.test.ts
@@ -1,4 +1,4 @@
-import {settled} from './promise';
+import {settled, timeout} from './promise';
 
 describe(settled.name, () => {
 	it('returns the resolved value', () => {
@@ -11,5 +11,25 @@ describe(settled.name, () => {
 		const promise = Promise.reject(1);
 
 		expect(settled(promise)).resolves.toStrictEqual([undefined, 1]);
+	});
+});
+
+describe(timeout.name, () => {
+	it('returns the resolved value', () => {
+		const promise = Promise.resolve(1);
+
+		expect(timeout(promise, Infinity)).resolves.toBe(1);
+	});
+
+	it('rejects if the given promise rejects', () => {
+		const promise = Promise.reject(1);
+
+		expect(timeout(promise, Infinity)).rejects.toBe(1);
+	});
+
+	it('times out if the given promise never resolves', () => {
+		const promise = new Promise(() => {});
+
+		expect(timeout(promise, 1)).rejects.toBe('Promise timed out');
 	});
 });
--- src/promise.ts
+++ src/promise.ts
@@ -22,3 +22,36 @@ export async function settled<E, T>(promise: Promise<T>): Promise<Result<T, E>> 
 		return [undefined, error as E];
 	}
 }
+
+const DELAY_RESOLVED_VALUE = Symbol('delayResolvedValue');
+
+function delay(ms: number): [timer: NodeJS.Timeout, delay: Promise<typeof DELAY_RESOLVED_VALUE>] {
+	let timer: NodeJS.Timeout;
+
+	const promise = new Promise<typeof DELAY_RESOLVED_VALUE>(resolve => {
+		timer = setTimeout(() => resolve(DELAY_RESOLVED_VALUE), ms);
+	});
+
+	return [timer!, promise];
+}
+
+/**
+ * Reject if the given promise does not resolve within the given timeout.
+ *
+ * @param promise - The promise to wait for
+ * @param timeoutMs - The timeout in milliseconds
+ *
+ * @returns The resolved value of the promise
+ */
+export async function timeout<T>(promise: Promise<T>, timeoutMs: number): Promise<T> {
+	const [timer, timeoutPromise] = delay(timeoutMs);
+	const resolvedValue = await Promise.race([promise, timeoutPromise]);
+
+	if (resolvedValue === DELAY_RESOLVED_VALUE) {
+		throw new Error('Timed out');
+	}
+
+	clearTimeout(timer);
+
+	return resolvedValue;
+}
