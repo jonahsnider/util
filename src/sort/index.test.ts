7e7ebeedbeec0ef5755deae51b2aee6f3fbd2465
Jonah Snider <jonah@jonahsnider.com>
2021-09-11 23:22:19 -0700

    feat(sort): add `isSorted` function

--- src/sort/index.test.ts
+++ src/sort/index.test.ts
@@ -1,4 +1,5 @@
-import {sortObject} from '.';
+import {isSorted, sortObject} from '.';
+import {ascending} from './compare';
 
 describe(sortObject.name, () => {
 	it('sorts', () => {
@@ -7,5 +8,19 @@ describe(sortObject.name, () => {
 		const sorted = sortObject(object, (a, b) => a - b);
 
 		expect(sorted).toStrictEqual(Object.entries({c: 1, b: 2, a: 3}));
+	});
+});
+
+describe(isSorted.name, () => {
+	it('works with empty arrays', () => {
+		expect(isSorted([], () => 1)).toBe(true);
+	});
+
+	it('works with sorted arrays', () => {
+		expect(isSorted([1, 1, 2, 3, 3], ascending)).toBe(true);
+	});
+
+	it('works with unsorted arrays', () => {
+		expect(isSorted([3, 3, 2, 1, 1], ascending)).toBe(false);
 	});
 });
--- src/sort/index.ts
+++ src/sort/index.ts
@@ -22,3 +22,33 @@ export * as Sort from './compare';
 export function sortObject<K extends PropertyKey, V>(object: Readonly<Record<K, V>>, compareFn: CompareFn<V>): Array<[K, V]> {
 	return Object.entries<V>(object).sort(([, aValue], [, bValue]) => compareFn(aValue, bValue)) as Array<[K, V]>;
 }
+
+/**
+ * Check whether an array is sorted according to a given compare function.
+ *
+ * @example
+ * ```js
+ * import { Sort } from '@jonahsnider/util';
+ *
+ * const array = [1, 2, 3, 4, 5];
+ *
+ * isSorted(array, (a, b) => a - b);
+ * ```
+ *
+ * @param array - Array to check
+ * @param compareFn - Function to compare elements
+ *
+ * @returns Whether the array is sorted according to the given compare function
+ */
+export function isSorted<T>(array: ArrayLike<T>, compareFn: CompareFn<T>): boolean {
+	for (let index = 0; index < array.length - 1; index++) {
+		const element = array[index];
+		const next = array[index + 1];
+
+		if (compareFn(element, next) > 0) {
+			return false;
+		}
+	}
+
+	return true;
+}
