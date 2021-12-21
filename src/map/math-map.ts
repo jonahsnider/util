0bbdc5a99464da6b3af5bd53fba6b7d7bf0b0647
Jonah Snider <jonah@jonahsnider.com>
2021-12-20 18:54:26 -0800

    docs(math-map): add doc comments

--- src/map/math-map.ts
+++ src/map/math-map.ts
@@ -4,9 +4,24 @@ import {DefaultMap} from './default-map';
 type V = number;
 
 /**
+ * A {@link DefaultMap} with methods for performing basic math operations that mutate the data.
+ *
  * @alpha
  */
 export class MathMap<K> extends DefaultMap<K, V> {
+	/**
+	 * Add an addend to the value for a given key.
+	 *
+	 * @example
+	 * ```js
+	 * map.add('a', 1);
+	 * ```
+	 *
+	 * @param key - The key to update the value of
+	 * @param addend - The value to add to the current value
+	 *
+	 * @returns The updated value
+	 */
 	add(key: K, addend: V): V {
 		const newValue = this.get(key) + addend;
 
@@ -15,10 +30,40 @@ export class MathMap<K> extends DefaultMap<K, V> {
 		return newValue;
 	}
 
+	/**
+	 * Subtract a subtrahend from the value for a given key.
+	 *
+	 * @example
+	 * ```js
+	 * map.subtract('a', 1);
+	 * ```
+	 *
+	 * @param key - The key to update the value of
+	 * @param subtrahend - The value to subtract from the current value
+	 *
+	 * @returns The updated value
+	 */
 	subtract(key: K, subtrahend: V): V {
-		return this.add(key, -subtrahend);
+		const newValue = this.get(key) - subtrahend;
+
+		this.set(key, newValue);
+
+		return newValue;
 	}
 
+	/**
+	 * Multiply a multiplicand by the value for a given key.
+	 *
+	 * @example
+	 * ```js
+	 * map.multiply('a', 1);
+	 * ```
+	 *
+	 * @param key - The key to update the value of
+	 * @param multiplicand - The value to multiply the current value by
+	 *
+	 * @returns The updated value
+	 */
 	multiply(key: K, multiplicand: V): V {
 		const newValue = this.get(key) * multiplicand;
 
@@ -27,6 +72,19 @@ export class MathMap<K> extends DefaultMap<K, V> {
 		return newValue;
 	}
 
+	/**
+	 * Divide the value for a given key by a divisor.
+	 *
+	 * @example
+	 * ```js
+	 * map.divide('a', 1);
+	 * ```
+	 *
+	 * @param key - The key to update the value of
+	 * @param divisor - The value to divide the current value by
+	 *
+	 * @returns The updated value
+	 */
 	divide(key: K, divisor: V): V {
 		const newValue = this.get(key) / divisor;
 
@@ -35,6 +93,19 @@ export class MathMap<K> extends DefaultMap<K, V> {
 		return newValue;
 	}
 
+	/**
+	 * Raise the value for a given key to an exponent.
+	 *
+	 * @example
+	 * ```js
+	 * map.pow('a', 2);
+	 * ```
+	 *
+	 * @param key - The key to update the value of
+	 * @param exponent - The exponent to raise the current value to
+	 *
+	 * @returns The updated value
+	 */
 	pow(key: K, exponent: V): V {
 		const newValue = this.get(key) ** exponent;
 
@@ -43,6 +114,18 @@ export class MathMap<K> extends DefaultMap<K, V> {
 		return newValue;
 	}
 
+	/**
+	 * Square root of the value for a given key.
+	 *
+	 * @example
+	 * ```js
+	 * map.sqrt('a');
+	 * ```
+	 *
+	 * @param key - The key to get the square root of
+	 *
+	 * @returns The square root of the value for `key`
+	 */
 	sqrt(key: K): V {
 		const newValue = Math.sqrt(this.get(key));
 
@@ -51,6 +134,19 @@ export class MathMap<K> extends DefaultMap<K, V> {
 		return newValue;
 	}
 
+	/**
+	 * Take the _n_th root of the value for a given key.
+	 *
+	 * @example
+	 * ```js
+	 * map.root('a', 2);
+	 * ```
+	 *
+	 * @param key - The key to get the _n_th root of
+	 * @param root - The root to get
+	 *
+	 * @returns The nth root of the value for `key`
+	 */
 	root(key: K, root: V): V {
 		if (root === 2) {
 			return this.sqrt(key);
