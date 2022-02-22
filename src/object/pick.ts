4b7dcfe9ad8cc8176b1921f1638118f4c70300f2
Jonah Snider <jonah@jonahsnider.com>
2022-02-22 11:40:37 -0800

    feat(object): add `pick` function

---
+++ src/object/pick.ts
@@ -0,0 +1,26 @@
+/**
+ * From `object`, pick a set of properties whose keys are in the `keys` array.
+ * This is an implementation of the `Pick<T, K>` type in TypeScript.
+ *
+ * Time complexity: _O(n)_
+ *
+ * Space complexity: _O(n)_
+ *
+ * @example
+ * ```js
+ * const object = { a: 1, b: 2, c: 3 };
+ *
+ * pick(object, ['a', 'b']); // { a: 1, b: 2 }
+ * ```
+ *
+ * @param object - The object you are renaming a key of
+ * @param keys - The keys to keep from the object
+ *
+ * @returns A new object containing the picked keys and corresponding values
+ *
+ * @public
+ * @category Object
+ */
+export function pick<T, K extends keyof T>(object: T, keys: readonly K[]): Pick<T, K> {
+	return Object.fromEntries(keys.map(key => [key, object[key]])) as Pick<T, K>;
+}
