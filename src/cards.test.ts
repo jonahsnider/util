ff25a311949d0f24949a0bb84e3121481fe228e9
Jonah Snider <jonah@jonahsnider.com>
2023-04-20 18:06:01 -0700

    fix(cards): `newDeck` function will not reuse `Card` objects in its return value

--- src/cards.test.ts
+++ src/cards.test.ts
@@ -1,7 +1,15 @@
 import {newDeck} from './cards';
+import {concatIterables} from './iterable';
 
 it('creates a new deck of cards', () => {
 	const deck = newDeck();
 
 	expect(deck.length).toBe(52);
+});
+
+it('does not reuse cards in a new deck', () => {
+	const deck1 = newDeck();
+	const deck2 = newDeck();
+
+	expect(new Set(concatIterables(deck1, deck2)).size).toBe(deck1.length + deck2.length);
 });
