c53fc8a37ef40e2c02c7320cb37fd5e546acfee6
Jonah Snider <jonah@jonahsnider.com>
2023-09-05 09:32:24 -0700

    refactor: comply with lint rules

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@
 
 // Configure Vitest (https://vitest.dev/config/)
 
+// eslint-disable-next-line import/no-extraneous-dependencies
 import {defineConfig} from 'vite';
 
 export default defineConfig({
