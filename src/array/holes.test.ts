fccc11dfce5c098cd72777469c00cd13748b1487
Jonah Snider <jonah@jonahsnider.com>
2025-07-18 23:26:14 -0700

    test: fix incorrect Biome refactor

--- src/array/holes.test.ts
+++ src/array/holes.test.ts
@@ -1,8 +1,10 @@
 import { holes } from './holes.js';
 
 it('finds holes', () => {
-	expect(holes([undefined, undefined])).toStrictEqual([0, 1]);
-	expect(holes([0, undefined, 2])).toStrictEqual([1]);
+	// biome-ignore lint/suspicious/noSparseArray: This is intentional
+	expect(holes([,])).toStrictEqual([0, 1]);
+	// biome-ignore lint/suspicious/noSparseArray: This is intentional
+	expect(holes([0, , 2])).toStrictEqual([1]);
 });
 
 it('returns empty array when there are no holes', () => {
