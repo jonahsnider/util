4946618601cdaffce09eef67d269c98f0f6167e0
Jonah Snider <jonah@jonahsnider.com>
2025-07-18 21:00:40 -0700

    style: format with Prettier

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import babel from '@rollup/plugin-babel';
 import typescript from '@rollup/plugin-typescript';
 import terser from '@rollup/plugin-terser';
-import { codecovRollupPlugin } from '@codecov/rollup-plugin';
+import {codecovRollupPlugin} from '@codecov/rollup-plugin';
 
 /** @type {import('@rollup/plugin-terser').Options} */
 const terserConfig = {
